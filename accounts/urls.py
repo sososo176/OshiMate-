46a98ce21f1624b0104b24f2f4063a3f2dca46bb
sososo176 <d2058144@gmail.com>
2025-06-19 01:13:54 +0900

    新規登録画面への遷移、新規登録画面の実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
+# accounts/urls.py の先頭に追記
+app_name = 'accounts'
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def login_view(request):
     return render(request, 'accounts/login.html')
 
+def signup_view(request):  # ←追加(6/18)
+    return render(request, 'accounts/signup.html')
+
 # Create your views here.
