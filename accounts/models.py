8da881cfc79f1271e43e063004ffb78d138e6dd5
sososo176 <d2058144@gmail.com>
2025-07-08 04:02:46 +0900

    シグナルコメントアウト

--- accounts/models.py
+++ accounts/models.py
@@ -7,10 +7,13 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)  # ← ここを追加
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True) 
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)  # ← これを追加
+    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)  
+    gender = models.CharField(max_length=10, choices=[('男性', '男性'), ('女性', '女性'), ('その他', 'その他')], blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+
 
     def __str__(self):
         return self.user.username
@@ -19,15 +22,15 @@ class Profile(models.Model):
 
 
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#     instance.profile.save()
 
 
-#新しいユーザーが登録されたときに、そのユーザーに対応する Profile を自動作成
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# #新しいユーザーが登録されたときに、そのユーザーに対応する Profile を自動作成
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
